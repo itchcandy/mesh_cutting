49c38b2778c1ed0811c14020713dd227a3bf45c8
Vishwakant Mishra <vkmishra2050@gmail.com>
2020-02-16 15:45:50 +0530

    Adding another animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Transform aim;
+    RaycastHit hit;
+    Ray ray;
+    Camera cam;
+
+    void Awake()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        var v = cam.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(cam.transform.position, Vector3.forward, Color.red, 1);
+    }
+
     void OnMouseDown()
     {
         
